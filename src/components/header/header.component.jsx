01b304dcf4dfaaf2cd96099aa5d63bae5ce2a34a
Souvik50 <duttasouvik916@yahoo.in>
2020-10-15 11:07:12 +0530

    integrated firebase with our application, created sign-in page and a button to signin using google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import './header.styles.scss';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => (
+const Header = ({ currentUser }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
         </Link>
         <div className='options'>
             <Link className='option' to='/shop'>SHOP</Link>
-            <Link className='option' to='contact'>CONTACT</Link>
+            <Link className='option' to='/contact'>CONTACT</Link>
+            {
+                currentUser ?
+                <Link className='option' to='#' onClick={() => auth.signOut()}>SIGN OUT</Link>
+                :
+                <Link className='option' to='signin'>SIGN IN</Link>
+            }
         </div>
     </div>
 )
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBU0hkqiqjgKKRLS5BEZ__wVjCV-1PtOLI",
+    authDomain: "crwn-db-e6cb5.firebaseapp.com",
+    databaseURL: "https://crwn-db-e6cb5.firebaseio.com",
+    projectId: "crwn-db-e6cb5",
+    storageBucket: "crwn-db-e6cb5.appspot.com",
+    messagingSenderId: "402275189519",
+    appId: "1:402275189519:web:0c0f0c049777dd7675347a",
+    measurementId: "G-WB97QNJDDN"
+  }
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
+
+export default firebase;
